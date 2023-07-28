c45c382be2ec2e3035a90383b8f4bceba29862e0
Alex Petrushchak <petrushchako@gmail.com>
2023-07-28 22:43:30 +0100

    DOM Node navigation (i.e. childrenNode)

--- JavaScript/Topic 4/Working with the DOM & Events/Nodes.js
+++ JavaScript/Topic 4/Working with the DOM & Events/Nodes.js
@@ -10,3 +10,27 @@ let counter = 0
 Array.from(jsDescription.childNodes).forEach(element =>{
     console.log("\nChild node : ", counter++ , " : ", element)
 })
+
+//Access child nodes and view key-value pairs
+console.log("\n\n\nAccessing childNodes : ", jsDescription.childNodes)
+
+//Access first and last child nodes
+console.log("\n\n\nAccess first child : ", jsDescription.firstChild.nodeValue)
+console.log("\nAccess last child : ", jsDescription.lastChild.nodeValue)
+
+for(let i=0; i<jsDescription.childNodes.length; i++){
+    let text = jsDescription.childNodes[i].nodeValue
+    if(text!==null){
+        console.log("\nChild node ", i , " : ", text)
+    }
+    else{
+        text = jsDescription.childNodes[i].firstChild.nodeValue
+        console.log("\nChild node ", i , " : ", text)
+    }
+    
+}
+
+
+
+let uList = document.getElementById("unOrder")
+console.log("\n\n\nAcessing children of Unordered List" , uList, "\n\n\nChild info : ", uList.childNodes)
