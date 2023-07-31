ed072774b7e4163bd4786bb8dc6017ffbf297dda
Alex Petrushchak <petrushchako@gmail.com>
2023-07-31 23:32:28 +0100

    Adjust code

--- JavaScript/Topic 4/Animations with JS/Animations.js
+++ JavaScript/Topic 4/Animations with JS/Animations.js
@@ -7,9 +7,9 @@ function animation(){
     let location1 = 450;
     let location2 = 0
     //Arguments are 1:Function that needs to be executed periodically, 2:frequency in msc
-    let time1 = setInterval(frame1, 10)
+    let time1 = setInterval(frame, 10)
 
-    function frame1() {
+    function frame() {
         if (location1 == 0){
             clearInterval(time1);
         } else {
