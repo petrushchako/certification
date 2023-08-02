4d82096312dee7fb9f4ad1fe3ce40c1e57ba6b85
Alex Petrushchak <petrushchako@gmail.com>
2023-08-02 22:21:53 +0100

    Add mouse events

--- JavaScript/Topic 4/Event Handlers for Click and Input/MultipleEventHandlers.js
+++ JavaScript/Topic 4/Event Handlers for Click and Input/MultipleEventHandlers.js
@@ -4,6 +4,11 @@ window.onload = function () {
     time.onclick = displayDate;
     convert.onchange = convertIntoUppercase;
     convert.onfocus = onFocus;
+    mouse.onmouseover = mouseOver;
+    mouse.onmouseout = mouseOut;
+    image.onmousedown = nightToronto;
+    image.onmouseup = dayToronto;  
+
 }
 
 function displayDate(){
@@ -19,4 +24,32 @@ function convertIntoUppercase(){
 function onFocus(){
     let focus = document.getElementById("convert")
     focus.style.background = "yellow"
+}
+
+function mouseOver() {
+    let mouseover = document.getElementById("mouse")
+    mouseover.style.backgroundColor = "green"
+    mouseover.innerHTML = "mouseover event"
+}
+
+function mouseOut() {
+    let mouseover = document.getElementById("mouse")
+    mouseover.style.backgroundColor = "white"
+    mouseover.innerHTML = "mouseout event"
+
+    setTimeout(function() {
+        // Code inside this callback function will be executed after 2 seconds
+        mouseover.style.backgroundColor = "pink";
+        mouseover.innerHTML = "Mouse Over Me";
+      }, 2000);
+}
+
+function nightToronto() {
+    let toronto = document.getElementById("image")
+    toronto.src = "../Toronto-at-night.png"
+}
+
+function dayToronto() {
+    let toronto = document.getElementById("image")
+    toronto.src = "../T.png"
 }
